495aae51f01f8f91e1f25859210379ab6ded9e23
Uchup <127828353+uchupbensin@users.noreply.github.com>
2023-07-03 21:30:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat array sekaligus inisialisasi nilai
+            string[] arrNama = { "Erick", "Wely", "Sony" };
+            int[] arrNim = { 1234, 1235, 1236 };
+            int[] arrNilai = { 75, 80, 90 };
+
+            Console.WriteLine("No\tNama\tNIM\tNilai");
+            Console.WriteLine("===============================");
+
+            //menampilkan semua elemen array
+            for (int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ".\t" + arrNama[i] + "\t" + arrNim[i] + "\t" + arrNilai[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
